f5c21d1e47964c33b18441b51550e1adc442f991
Matt Foster <matt@fostermatt.com>
2016-02-09 16:07:14 -0800

    basic functionality working now

--- zappolator/ViewController.swift
+++ zappolator/ViewController.swift
@@ -11,104 +11,97 @@ import UIKit
 
 class ViewController: UIViewController {
 	
-	
-	// inputOp: 0=none, +=add, -=sub, *=mul, /=div
-	var inputOp:Int = 0
-	
-	// Display string
-	var labelString:String = "0"
+	var isFirstDigit = true
+	// inputOp: ==none, +=add, -=sub, ×=mul, /=div
+	var inputOp:String = "="
 	// first input
-	var savedNum:Float32 = 0.0
+	var savedNum:Double = 0.0
 	// current/second input
-	var labelNum:Float32 = 0.0
-	var decimalEntered:Bool = false
-	var lastButtonWasMode:Bool = false
-	
-	func pressedNumber(num:String){
-		
-	}
-	
-	
-	func updateDisplay(){
-		
-	}
-	
-	func changeMode(newMode:Int){
-		if savedNum == 0.0 {
-			return
+	var displayVal:Double {
+		get{
+			return NSNumberFormatter().numberFromString(displayLabel.text!)!.doubleValue
 		}
-		print(newMode)
-		inputOp = newMode
-		lastButtonWasMode = true
-	}
-	
-	func clearDisplay(){
-		labelString = "0"
-		labelNum = 0
-		updateDisplay()
+		set {
+			displayLabel.text = "\(newValue)"
+			isFirstDigit = true
+			inputOp = "="
+		}
 	}
 
-	override func viewDidLoad() {
-		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
-	}
-
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
+	func allClear(){
+		savedNum = 0
+		displayVal = 0
+		inputOp = "="
+		isFirstDigit = true
 	}
 	
 	@IBOutlet weak var displayLabel: UILabel!
 	
-	@IBAction func pressedAllClear(sender: AnyObject) {
-		savedNum = 0
-		inputOp = 0
-		lastButtonWasMode = false
-		clearDisplay()
+	@IBAction func pressedNumber(sender: UIButton){
+		let myNum = sender.currentTitle!
+		displayLabel.text = isFirstDigit ? myNum : displayLabel.text! + myNum
+		isFirstDigit = false
 	}
 	
-	@IBAction func pressedClear(sender: AnyObject) {clearDisplay()}
+	@IBAction func pressedAllClear(sender: AnyObject) {
+		allClear()
+	}
+	
+	@IBAction func pressedClear(sender: AnyObject) {
+		let tempOp = inputOp
+		displayVal = 0
+		inputOp = tempOp
+		isFirstDigit = true
+	}
 	
 	@IBAction func pressedNegate(sender: AnyObject) {
+		displayVal = displayVal * -1
 	}
 	
-	@IBAction func pressedPercent(sender: AnyObject) {
+	@IBAction func pressedOperand(sender: UIButton) {
+		inputOp = sender.currentTitle!
+		print(displayVal)
+		savedNum = displayVal
+		isFirstDigit = true
 	}
-	
-	@IBAction func pressedDecimal(sender: AnyObject) {
-		
-	}
-	
-	@IBAction func pressed0(sender: AnyObject) {pressedNumber("0")}
-	
-	@IBAction func pressed1(sender: AnyObject) {pressedNumber("1")}
-	
-	@IBAction func pressed2(sender: AnyObject) {pressedNumber("2")}
-	
-	@IBAction func pressed3(sender: AnyObject) {pressedNumber("3")}
-	
-	@IBAction func pressed4(sender: AnyObject) {pressedNumber("4")}
-	
-	@IBAction func pressed5(sender: AnyObject) {pressedNumber("5")}
-	
-	@IBAction func pressed6(sender: AnyObject) {pressedNumber("6")}
-	
-	@IBAction func pressed7(sender: AnyObject) {pressedNumber("7")}
-	
-	@IBAction func pressed8(sender: AnyObject) {pressedNumber("8")}
-	
-	@IBAction func pressed9(sender: AnyObject) {pressedNumber("9")}
-	
-	@IBAction func pressedAdd(sender: AnyObject) {changeMode(1)}
-	
-	@IBAction func pressedSubtract(sender: AnyObject) {changeMode(2)}
-	
-	@IBAction func pressedMultiply(sender: AnyObject) {changeMode(3)}
-	
-	@IBAction func pressedDivide(sender: AnyObject) {changeMode(4)}
 	
 	@IBAction func pressedEqual(sender: AnyObject) {
-		
+//		print(savedNum)
+//		print(inputOp)
+//		print(displayVal)
+//		print(sender.currentTitle!)
+		if inputOp == "+"{
+			displayVal = savedNum + displayVal
+		}
+		else if inputOp == "-"{
+			displayVal = savedNum - displayVal
+		}
+		else if inputOp == "×"{
+			displayVal = savedNum * displayVal
+		}
+		else if inputOp == "÷"{
+			if displayVal == 0{
+				allClear()
+				displayLabel.text = "Error"
+			}
+//			else if savedNum == 0{
+//				allClear()
+//				displayLabel.text = "∞"
+//			}
+			else {
+				displayVal = savedNum / displayVal
+			}
+		}
+	}
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+	
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
 	}
 
 
